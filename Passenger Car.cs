371707b82367ac5fe4e08a40e81f97c6475535fa
Redinkton <104201122+Redinkton@users.noreply.github.com>
2022-08-16 14:12:56 +0300

    Add files via upload

---
+++ Passenger Car.cs
@@ -0,0 +1,37 @@
+﻿namespace Vehicle
+{
+    internal class Passenger_Car : Car
+    {
+        public Passenger_Car()
+        {
+            AverageFuelConsumption = 12;
+            FuelTank = 42;
+            Speed = 90;
+        }
+        public int People { get; set; }
+        public  void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("Сколько человек с собой возьмешь?");
+            People = int.Parse(Console.ReadLine());
+            double i = 0;
+            if (People >= 1 & People <= 4)
+            {
+                Console.Clear();
+                Console.WriteLine("Отлчично, должны поместиться!");
+                i =SensorWithPeople(People);
+                TimeLeft(FuelTank, Speed, AverageFuelConsumption,i);
+            }
+            else if (People == 0)
+            {
+                i = SensorEmpty();
+                TimeLeft(FuelTank, Speed, AverageFuelConsumption,i);
+            }
+            else
+            {
+                Console.WriteLine("Нельзя столько человек в машину");
+            }
+        }
+
+    }
+}
---
+++ Sport Car.cs
@@ -0,0 +1,19 @@
+﻿namespace Vehicle
+{
+    internal class Sport_Car : Car
+    {
+        public Sport_Car()
+        {
+            AverageFuelConsumption = 15.6;
+            FuelTank = 37;
+            Speed = 180;
+        }
+        public void Start()
+        {
+            Console.Clear();
+            SensorEmpty();
+            Console.WriteLine("Сейчас поедем дрифтовать!");
+        }
+    }
+
+}
